3e860297ed3266f88baae4bdb5d5bba07b5d1b4e
Durian <1323807496@qq.com>
2020-02-17 12:13:09 +0800

    完成config配置中心集成

---
+++ tensquare_base/src/main/java/com/tensquare/base/pojo/Test.java
@@ -0,0 +1,14 @@
+package com.tensquare.base.pojo;
+
+/**
+ * Author: Durian
+ * Date: 2020/2/16 18:19
+ * Description:
+ */
+public class Test
+{
+    public static void main(String[] args)
+    {
+        System.out.println("......");
+    }
+}
